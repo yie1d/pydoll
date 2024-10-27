a3b14541a527da7e8e70b42c212771dc818e81e7
Thalison Fernandes <thalissfernandes99@gmail.com>
2024-10-27 12:50:52 -0300

    docs: enhance Options class with detailed docstrings for clarity

--- pydoll/browser/options.py
+++ pydoll/browser/options.py
@@ -1,21 +1,61 @@
 class Options:
+    """
+    A class to manage command-line options for a browser instance.
+
+    This class allows the user to specify command-line arguments and
+    the binary location of the browser executable.
+    """
+
     def __init__(self):
+        """
+        Initializes the Options instance.
+
+        Sets up an empty list for command-line arguments and a string 
+        for the binary location of the browser.
+        """
         self._arguments = []
         self._binary_location = ''
 
     @property
     def arguments(self) -> list:
+        """
+        Gets the list of command-line arguments.
+
+        Returns:
+            list: A list of command-line arguments added to the options.
+        """
         return self._arguments
 
     @property
     def binary_location(self) -> str:
+        """
+        Gets the location of the browser binary.
+
+        Returns:
+            str: The file path to the browser executable.
+        """
         return self._binary_location
 
     @binary_location.setter
     def binary_location(self, location: str):
+        """
+        Sets the location of the browser binary.
+
+        Args:
+            location (str): The file path to the browser executable.
+        """
         self._binary_location = location
 
     def add_argument(self, argument: str):
+        """
+        Adds a command-line argument to the options.
+
+        Args:
+            argument (str): The command-line argument to be added.
+
+        Raises:
+            ValueError: If the argument is already in the list of arguments.
+        """
         if argument not in self.arguments:
             self.arguments.append(argument)
         else:
