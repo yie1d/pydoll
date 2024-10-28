5e2edc5ff79b08766f5419b5239aa3addc91b9c9
Thalison Fernandes <thalissfernandes99@gmail.com>
2024-10-28 17:33:12 -0300

    feat: implement some basic methods to navigate and control the browser instance

--- pydoll/browser/base.py
+++ pydoll/browser/base.py
@@ -1,12 +1,16 @@
 import asyncio
 import os
 import subprocess
-from typing import Callable
 from abc import ABC, abstractmethod
+from typing import Callable
 
 from pydoll.browser.options import Options
 from pydoll.commands.browser import BrowserCommands
+from pydoll.commands.dom import DomCommands
+from pydoll.commands.page import PageCommands
 from pydoll.connection import ConnectionHandler
+from pydoll.element import WebElement
+from pydoll.utils import decode_image_to_bytes
 
 
 class Browser(ABC):
@@ -25,11 +29,11 @@ class Browser(ABC):
             options (Options | None): An instance of the Options class to
             configure the browser. If None, default options will be used.
         """
-        self.options = self._initialize_options(options)
-        self.process = self._start_browser()
         self.connection_handler = ConnectionHandler()
+        self.options = self._initialize_options(options)
+        self.process = None
 
-    def start(self) -> subprocess.Popen:
+    async def start(self) -> None:
         """
         Starts the browser process with the specified options.
 
@@ -39,25 +43,36 @@ class Browser(ABC):
         binary_location = (
             self.options.binary_location or self._get_default_binary_location()
         )
-        return subprocess.Popen(
-            [binary_location, '--remote-debugging-port=9222', *self.options.arguments],
+        self.process = subprocess.Popen(
+            [
+                binary_location,
+                '--remote-debugging-port=9222',
+                *self.options.arguments,
+            ],
             stdout=subprocess.PIPE,
         )
+        if not await self._is_browser_running():
+            raise ValueError('Failed to start browser')
 
-    def stop(self):
+    async def enable_page_events(self):
+        await self.connection_handler.execute_command(
+            PageCommands.enable_page()
+        )
+
+    async def stop(self):
         """
         Stops the running browser process.
 
         Raises:
             ValueError: If the browser is not currently running.
         """
-        if self._is_browser_running():
+        if await self._is_browser_running():
             self.process.terminate()
-            self._execute_sync_command(BrowserCommands.CLOSE)
+            await self._execute_command(BrowserCommands.CLOSE)
         else:
             raise ValueError('Browser is not running')
 
-    def get_screenshot(self, path: str):
+    async def get_screenshot(self, path: str):
         """
         Captures a screenshot of the current page and saves
         it to the specified path.
@@ -65,12 +80,110 @@ class Browser(ABC):
         Args:
             path (str): The file path where the screenshot will be saved.
         """
-        response = self._execute_sync_command(BrowserCommands.SCREENSHOT)
-        image_bytes = response['result']['data'].encode('utf-8')
+        response = await self._execute_command(PageCommands.screenshot())
+        image_b64 = response['result']['data'].encode('utf-8')
+        image_bytes = decode_image_to_bytes(image_b64)
         with open(path, 'wb') as file:
             file.write(image_bytes)
 
-    def on(self, event_name: str, callback: Callable):
+    async def go_to(self, url: str):
+        """
+        Navigates the browser to the specified URL.
+
+        Args:
+            url (str): The URL to navigate to.
+        """
+        await self._execute_command(PageCommands.go_to(url))
+
+    async def refresh(self):
+        """
+        Refreshes the current page in the browser.
+        """
+        await self._execute_command(PageCommands.refresh())
+
+    async def get_window_id(self):
+        """
+        Retrieves the ID of the current browser window.
+
+        Returns:
+            str: The ID of the current browser window.
+        """
+        response = await self._execute_command(BrowserCommands.get_window_id())
+        return response['result']['windowId']
+
+    async def set_window_bounds(self, bounds: dict):
+        """
+        Sets the bounds of the specified window.
+
+        Args:
+            bounds (dict): The bounds to set for the window.
+        """
+        window_id = await self.get_window_id()
+        await self._execute_command(
+            BrowserCommands.set_window_bounds(window_id, bounds)
+        )
+
+    async def set_window_maximized(self):
+        """
+        Maximizes the specified window.
+        """
+        window_id = await self.get_window_id()
+        await self._execute_command(
+            BrowserCommands.set_window_maximized(window_id)
+        )
+
+    async def set_window_minimized(self):
+        """
+        Minimizes the specified window.
+        """
+        window_id = await self.get_window_id()
+        await self._execute_command(
+            BrowserCommands.set_window_minimized(window_id)
+        )
+
+    async def print_to_pdf(self, path: str):
+        """
+        Prints the current page to a PDF file and saves it to the specified path.
+
+        Args:
+            path (str): The file path where the PDF will be saved.
+        """
+        response = await self._execute_command(PageCommands.print_to_pdf())
+        pdf_b64 = response['result']['data'].encode('utf-8')
+        pdf_bytes = decode_image_to_bytes(pdf_b64)
+        with open(path, 'wb') as file:
+            file.write(pdf_bytes)
+
+    async def _get_root_node_id(self):
+        response = await self._execute_command(DomCommands.dom_document())
+        return response['result']['root']['nodeId']
+
+    async def _describe_node(self, node_id: int):
+        response = await self._execute_command(
+            DomCommands.describe_node(node_id)
+        )
+        return response['result']['node']
+
+    async def find_element(self, by: DomCommands.SelectorType, value: str):
+        """
+        Finds an element on the current page using the specified selector.
+
+        Args:
+            by (str): The type of selector to use (e.g., 'css', 'xpath').
+            value (str): The value of the selector to use.
+
+        Returns:
+            dict: The response from the browser.
+        """
+        root_node_id = await self._get_root_node_id()
+        response = await self._execute_command(
+            DomCommands.find_element(root_node_id, by, value)
+        )
+        target_node_id = response['result']['nodeId']
+        node_description = await self._describe_node(target_node_id)
+        return WebElement(node_description, self.connection_handler)
+
+    async def on(self, event_name: str, callback: Callable):
         """
         Registers an event callback for a specific event.
 
@@ -78,46 +191,39 @@ class Browser(ABC):
             event_name (str): Name of the event to listen for.
             callback (Callable): function to be called when the event occurs.
         """
-        asyncio.run(
-            self.connection_handler.register_callback(event_name, callback)
-        )
+        await self.connection_handler.register_callback(event_name, callback)
 
-    def _start_browser(self) -> subprocess.Popen:
+    async def _is_browser_running(self):
         """
-        Starts the browser and verifies if it is running.
-
-        Raises:
-            ValueError: If the browser fails to start.
+        Checks if the browser process is currently running.
+        Attempts to connect to the browser to verify its status.
 
         Returns:
-            subprocess.Popen: The process object for the started browser.
+            bool: True if the browser is running, False otherwise.
         """
-        process = self.start()
-        if not self._is_browser_running():
-            raise ValueError('Failed to start browser')
-        return process
+        for _ in range(10):
+            if await self._check_browser_connection():
+                return True
+            await asyncio.sleep(1)
+        return False
 
-    @abstractmethod
-    def _get_default_binary_location(self) -> str:
-        """
-        Retrieves the default location of the browser binary.
-
-        This method must be implemented by subclasses.
-        """
-        pass
-
-    def _is_browser_running(self):
+    async def _check_browser_connection(self):
         """
         Checks if the browser process is currently running.
 
         Returns:
             bool: True if the browser is running, False otherwise.
         """
-        return self.process.poll() is None
+        try:
+            await self.connection_handler.browser_ws_address
+            return True
+        except Exception as exc:
+            print(f'Browser is not running: {exc}')
+            return False
 
-    def _execute_sync_command(self, command: str):
+    async def _execute_command(self, command: str):
         """
-        Executes a command synchronously through the connection handler.
+        Executes a command through the connection handler.
 
         Args:
             command (str): The command to be executed.
@@ -125,7 +231,7 @@ class Browser(ABC):
         Returns:
             The response from executing the command.
         """
-        return asyncio.run(self.connection_handler.execute_command(command))
+        return await self.connection_handler.execute_command(command)
 
     @staticmethod
     def _validate_browser_path(path: str):
@@ -164,3 +270,12 @@ class Browser(ABC):
         if not isinstance(options, Options):
             raise ValueError('Invalid options')
         return options
+
+    @abstractmethod
+    def _get_default_binary_location(self) -> str:
+        """
+        Retrieves the default location of the browser binary.
+
+        This method must be implemented by subclasses.
+        """
+        pass
