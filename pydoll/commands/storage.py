e63f6778dcd4327d42e2447e4313130a8c5bc552
Thalison Fernandes <thalissfernandes99@gmail.com>
2024-11-04 20:48:28 -0300

    refactor: remove unnecessary blank lines in storage.py for clarity

--- pydoll/commands/storage.py
+++ pydoll/commands/storage.py
@@ -6,13 +6,13 @@ class StorageCommands:
     @classmethod
     def clear_cookies(cls) -> dict:
         return cls.CLEAR_COOKIES
-    
+
     @classmethod
     def set_cookies(cls, cookies: list) -> dict:
         set_cookies = cls.SET_COOKIES.copy()
         set_cookies['params']['cookies'] = cookies
         return set_cookies
-    
+
     @classmethod
     def get_cookies(cls) -> dict:
         return cls.GET_COOKIES
