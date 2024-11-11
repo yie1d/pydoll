6fa14d42ff8fb527d0e63b7b4e11d88271b8dfd6
Thalisson Fernandes <thalissfernandes99@gmail.com>
2024-11-11 13:02:18 -0300

    feat: add JavaScript functions for element visibility and interaction

--- pydoll/constants.py
+++ pydoll/constants.py
@@ -7,3 +7,54 @@ class By(str, Enum):
     CLASS_NAME = 'class_name'
     ID = 'id'
     TAG_NAME = 'tag_name'
+
+
+class Scripts:
+    ELEMENT_VISIBLE = """
+    function() {
+        const rect = this.getBoundingClientRect();
+        return (
+            rect.width > 0 && rect.height > 0
+            && getComputedStyle(this).visibility !== 'hidden'
+            && getComputedStyle(this).display !== 'none'
+        )
+    }
+    """
+
+    ELEMENT_ON_TOP = """
+    function() {
+        const rect = this.getBoundingClientRect();
+        const elementFromPoint = document.elementFromPoint(
+            rect.x + rect.width / 2,
+            rect.y + rect.height / 2
+        );
+        return elementFromPoint === this;
+    }
+    """
+
+    CLICK = """
+    function(){
+        clicked = false;
+        this.addEventListener('click', function(){
+            clicked = true;
+        });
+        this.click();
+        return clicked;
+    }
+    """
+
+    CLICK_OPTION_TAG = """
+    document.querySelector('option[value="{self.value}"]').selected = true;
+    var selectParentXpath = (
+        '//option[@value="{self.value}"]//ancestor::select'
+    );
+    var select = document.evaluate(
+        selectParentXpath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+    ).singleNodeValue;
+    var event = new Event('change', { bubbles: true });
+    select.dispatchEvent(event);
+    """
