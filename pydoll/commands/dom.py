cfad1bc23fedc3c874d5f3b1ddd350f28d2009f1
Thalisson Fernandes <thalissfernandes99@gmail.com>
2025-02-05 03:20:02 -0300

    refactor: add command imports and remove obsolete connection handler code

--- pydoll/commands/dom.py
+++ pydoll/commands/dom.py
@@ -17,7 +17,7 @@ class DomCommands:
         SelectorType (Literal): A type definition for supported selector types.
     """
 
-    SelectorType = Literal[By.CSS, By.XPATH, By.CLASS_NAME, By.ID, By.TAG_NAME]
+    SelectorType = Literal[By.CSS_SELECTOR, By.XPATH, By.CLASS_NAME, By.ID, By.TAG_NAME]
 
     ENABLE = {'method': 'DOM.enable'}
     DOM_DOCUMENT = {'method': 'DOM.getDocument'}
