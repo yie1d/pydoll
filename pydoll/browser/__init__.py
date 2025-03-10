29836cf00b9eb2f51b46ecc9d1ff5f9da8430063
Herio <haohe@buaa.edu.cn>
2025-03-10 23:04:07 +0800

    feat: Add Microsoft Edge browser support

---
+++ pydoll/browser/__init__.py
@@ -0,0 +1,4 @@
+from pydoll.browser.chrome import Chrome
+from pydoll.browser.edge import Edge
+
+__all__ = ['Chrome', 'Edge']
---
+++ pydoll/browser/edge.py
@@ -0,0 +1,63 @@
+import platform
+
+from pydoll.browser.base import Browser
+from pydoll.browser.managers import BrowserOptionsManager
+from pydoll.browser.options import Options
+from pydoll.exceptions import BrowserNotRunning
+
+
+class Edge(Browser):
+    def __init__(
+        self, options: Options | None = None, connection_port: int = 9222
+    ):
+        if options is None:
+            options = Options()
+            
+        # Add Edge-specific startup arguments
+        options.add_argument('--no-first-run')  # Disable first run experience
+        options.add_argument('--no-default-browser-check')  # Disable default browser check
+        options.add_argument('--disable-crash-reporter')  # Disable crash reporting
+        options.add_argument('--disable-features=TranslateUI')  # Disable translation UI
+        options.add_argument('--disable-component-update')  # Disable component updates
+        options.add_argument('--disable-background-networking')  # Disable background networking
+        
+        # Add debugging parameters
+        options.add_argument(f'--remote-debugging-port={connection_port}')
+        options.add_argument('--remote-allow-origins=*')
+
+        # Initialize base class first so we can use its methods
+        super().__init__(options, connection_port)
+        
+        # Setup user data directory if not already set
+        if not any('--user-data-dir=' in arg for arg in self.options.arguments):
+            self._setup_user_dir()
+
+    @staticmethod
+    def _get_default_binary_location():
+        """
+        Get the default Edge browser binary location based on the operating system.
+        
+        Returns:
+            str: Path to the Edge browser executable
+            
+        Raises:
+            ValueError: If the operating system is not supported
+        """
+        os_name = platform.system()
+        browser_paths = {
+            'Windows':
+                r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',
+            'Linux':
+                '/usr/bin/microsoft-edge',
+            'Darwin':
+                '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge'
+        }
+
+        browser_path = browser_paths.get(os_name)
+
+        if not browser_path:
+            raise ValueError('Unsupported operating system')
+
+        return BrowserOptionsManager.validate_browser_path(
+            browser_path
+        )
