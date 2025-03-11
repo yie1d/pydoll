6bb809d050f73f03ffc8251439476bac0b7db7a4
Danny <dimitrys@rafael.co.il>
2025-03-11 17:13:31 +0200

    Revert "Enhance Chrome binary detection for Windows"
    This reverts commit 684db08f6704dd7d510820b9daebc0542191bc05.

--- pydoll/browser/chrome.py
+++ pydoll/browser/chrome.py
@@ -1,4 +1,3 @@
-import os
 import platform
 
 from pydoll.browser.base import Browser
@@ -15,21 +14,20 @@ class Chrome(Browser):
     @staticmethod
     def _get_default_binary_location():
         os_name = platform.system()
-
-        if os_name == 'Windows':
-            possible_paths = [
+        browser_paths = {
+            'Windows':
                 r'C:\Program Files\Google\Chrome\Application\chrome.exe',
-                r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-            ]
-            for path in possible_paths:
-                if os.path.exists(path):
-                    return BrowserOptionsManager.validate_browser_path(path)
-            raise ValueError("Chrome not found in default Windows locations.")
-        elif os_name == 'Linux':
-            browser_path = '/usr/bin/google-chrome'
-        elif os_name == 'Darwin':
-            browser_path = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-        else:
+            'Linux':
+                '/usr/bin/google-chrome',
+            'Darwin':
+                '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        }
+
+        browser_path = browser_paths.get(os_name)
+
+        if not browser_path:
             raise ValueError('Unsupported OS')
 
-        return BrowserOptionsManager.validate_browser_path(browser_path)
+        return BrowserOptionsManager.validate_browser_path(
+            browser_path
+        )
