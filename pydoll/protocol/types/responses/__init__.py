1d68bc7d8a1d31698fd2572935c66c382eb9f994
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-04-27 01:32:01 -0300

    refactor: simplify import statements by using wildcard imports for responses

--- pydoll/protocol/types/responses/__init__.py
+++ pydoll/protocol/types/responses/__init__.py
@@ -1,49 +1,5 @@
-from pydoll.protocol.types.responses.base_responses_types import (
-    Response,
-    ResponseResult,
-)
-from pydoll.protocol.types.responses.browser_responses_types import (
-    GetVersionResponse,
-    GetWindowForTargetResponse,
-)
-from pydoll.protocol.types.responses.fetch_responses_types import (
-    GetResponseBodyResponse,
-    TakeResponseBodyAsStreamResponse,
-)
-from pydoll.protocol.types.responses.network_responses_types import (
-    CanClearBrowserCacheResponse,
-    CanClearBrowserCookiesResponse,
-    CanEmulateNetworkConditionsResponse,
-    Cookie,
-    GetCertificateResponse,
-    GetCookiesResponse,
-    GetRequestPostDataResponse,
-    GetResponseBodyForInterceptionResponse,
-    GetResponseBodyResponse,
-    SearchInResponseBodyResponse,
-    SetCookieResponse,
-    StreamResourceContentResponse,
-    TakeResponseBodyForInterceptionAsStreamResponse,
-)
-
-__all__ = [
-    'Response',
-    'ResponseResult',
-    'GetWindowForTargetResponse',
-    'GetVersionResponse',
-    'GetResponseBodyResponse',
-    'TakeResponseBodyAsStreamResponse',
-    'CanClearBrowserCacheResponse',
-    'CanClearBrowserCookiesResponse',
-    'CanEmulateNetworkConditionsResponse',
-    'Cookie',
-    'GetCertificateResponse',
-    'GetCookiesResponse',
-    'GetRequestPostDataResponse',
-    'GetResponseBodyForInterceptionResponse',
-    'GetResponseBodyResponse',
-    'SearchInResponseBodyResponse',
-    'SetCookieResponse',
-    'StreamResourceContentResponse',
-    'TakeResponseBodyForInterceptionAsStreamResponse',
-]
+from pydoll.protocol.types.responses.base_responses_types import *
+from pydoll.protocol.types.responses.browser_responses_types import *
+from pydoll.protocol.types.responses.fetch_responses_types import *
+from pydoll.protocol.types.responses.network_responses_types import *
+from pydoll.protocol.types.responses.page_responses_types import *
