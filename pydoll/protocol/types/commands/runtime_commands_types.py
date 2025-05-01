b7c045a0c50833837b0f47453a1bbdcdafcc916b
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-04-30 23:11:26 -0300

    feat: add initial runtime command types for protocol handling

---
+++ pydoll/protocol/types/commands/runtime_commands_types.py
@@ -0,0 +1,125 @@
+from typing import List, NotRequired, TypedDict, Union
+
+from pydoll.constants import SerializationValue, UnserializableEnum
+from pydoll.protocol.types.commands import CommandParams
+
+
+class CallArgument(TypedDict):
+    value: NotRequired[str]
+    unserializableValue: NotRequired[Union[UnserializableEnum, str]]
+    objectId: NotRequired[str]
+
+
+class SerializationOptions(TypedDict):
+    serialization: SerializationValue
+    maxDepth: NotRequired[int]
+    additionalParameters: NotRequired[dict]
+
+
+class AddBindingParams(CommandParams):
+    name: str
+    executionContextName: NotRequired[str]
+
+
+class AwaitPromiseParams(CommandParams):
+    promiseObjectId: str
+    returnByValue: NotRequired[bool]
+    generatePreview: NotRequired[bool]
+
+
+class CallFunctionOnParams(CommandParams):
+    functionDeclaration: str
+    objectId: NotRequired[str]
+    arguments: NotRequired[List[CallArgument]]
+    silent: NotRequired[bool]
+    returnByValue: NotRequired[bool]
+    generatePreview: NotRequired[bool]
+    userGesture: NotRequired[bool]
+    awaitPromise: NotRequired[bool]
+    executionContextId: NotRequired[str]
+    objectGroup: NotRequired[str]
+    throwOnSideEffect: NotRequired[bool]
+    uniqueContextId: NotRequired[str]
+    serializationOptions: NotRequired[SerializationOptions]
+
+
+class CompileScriptParams(CommandParams):
+    expression: str
+    sourceURL: NotRequired[str]
+    persistScript: NotRequired[bool]
+    executionContextId: NotRequired[str]
+
+
+class EvaluateParams(CommandParams):
+    expression: str
+    objectGroup: NotRequired[str]
+    includeCommandLineAPI: NotRequired[bool]
+    silent: NotRequired[bool]
+    contextId: NotRequired[str]
+    returnByValue: NotRequired[bool]
+    generatePreview: NotRequired[bool]
+    userGesture: NotRequired[bool]
+    awaitPromise: NotRequired[bool]
+    throwOnSideEffect: NotRequired[bool]
+    timeout: NotRequired[float]
+    disableBreaks: NotRequired[bool]
+    replMode: NotRequired[bool]
+    allowUnsafeEvalBlockedByCSP: NotRequired[bool]
+    uniqueContextId: NotRequired[str]
+    serializationOptions: NotRequired[SerializationOptions]
+
+
+class GetPropertiesParams(CommandParams):
+    objectId: str
+    ownProperties: NotRequired[bool]
+    accessorPropertiesOnly: NotRequired[bool]
+    generatePreview: NotRequired[bool]
+    nonIndexedPropertiesOnly: NotRequired[bool]
+
+
+class GlobalLexicalScopeNamesParams(CommandParams):
+    executionContextId: NotRequired[str]
+
+
+class QueryObjectsParams(CommandParams):
+    prototypeObjectId: str
+    objectGroup: NotRequired[str]
+
+
+class ReleaseObjectParams(CommandParams):
+    objectId: str
+
+
+class ReleaseObjectGroupParams(CommandParams):
+    objectGroup: str
+
+
+class RemoveBindingParams(CommandParams):
+    name: str
+
+
+class RunScriptParams(CommandParams):
+    scriptId: str
+    executionContextId: NotRequired[str]
+    objectGroup: NotRequired[str]
+    silent: NotRequired[bool]
+    includeCommandLineAPI: NotRequired[bool]
+    returnByValue: NotRequired[bool]
+    generatePreview: NotRequired[bool]
+    awaitPromise: NotRequired[bool]
+
+
+class SetAsyncCallStackDepthParams(CommandParams):
+    maxDepth: int
+
+
+class GetExceptionDetailsParams(CommandParams):
+    errorObjectId: str
+
+
+class SetCustomObjectFormatterEnabledParams(CommandParams):
+    enabled: bool
+
+
+class SetMaxCallStackSizeToCaptureParams(CommandParams):
+    size: int
