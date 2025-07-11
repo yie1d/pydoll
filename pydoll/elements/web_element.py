120746672a5ff203478a8a05f8c0d15268bb6f7c
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-07-11 20:14:46 -0300

    fix: improve selection script for higher accuracy

--- pydoll/elements/web_element.py
+++ pydoll/elements/web_element.py
@@ -346,7 +346,11 @@ class WebElement(FindElementsMixin):  # noqa: PLR0904
     async def _click_option_tag(self):
         """Specialized method for clicking <option> elements in dropdowns."""
         script = Scripts.CLICK_OPTION_TAG.replace('{self.value}', self.value)
-        await self._execute_command(RuntimeCommands.evaluate(script))
+        await self._execute_command(RuntimeCommands.call_function_on(
+            object_id=self._object_id,
+            function_declaration=script,
+            return_by_value=True,
+        ))
 
     async def _is_element_visible(self):
         """Check if element is visible using comprehensive JavaScript visibility test."""
