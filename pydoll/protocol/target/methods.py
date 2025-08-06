877f7b7cfb3bc78bb394ed592f15baac5cd9851a
Thalison Fernandes <thalissfernandes99@gmail.com>
2025-08-06 00:53:23 -0300

    refactor: improve protocol types for target domain

--- pydoll/protocol/target/methods.py
+++ pydoll/protocol/target/methods.py
@@ -1,21 +1,260 @@
 from enum import Enum
 
+from typing_extensions import NotRequired, TypedDict
+
+from pydoll.protocol.base import Command, EmptyParams, EmptyResponse, Response
+from pydoll.protocol.browser.types import BrowserContextID
+
+from .types import (
+    RemoteLocation,
+    SessionID,
+    TargetFilter,
+    TargetID,
+    TargetInfo,
+    WindowState,
+)
+
 
 class TargetMethod(str, Enum):
+    """Target domain method names."""
+
     ACTIVATE_TARGET = 'Target.activateTarget'
     ATTACH_TO_TARGET = 'Target.attachToTarget'
+    ATTACH_TO_BROWSER_TARGET = 'Target.attachToBrowserTarget'
     CLOSE_TARGET = 'Target.closeTarget'
+    EXPOSE_DEV_TOOLS_PROTOCOL = 'Target.exposeDevToolsProtocol'
     CREATE_BROWSER_CONTEXT = 'Target.createBrowserContext'
+    GET_BROWSER_CONTEXTS = 'Target.getBrowserContexts'
     CREATE_TARGET = 'Target.createTarget'
     DETACH_FROM_TARGET = 'Target.detachFromTarget'
     DISPOSE_BROWSER_CONTEXT = 'Target.disposeBrowserContext'
-    GET_BROWSER_CONTEXTS = 'Target.getBrowserContexts'
-    GET_TARGETS = 'Target.getTargets'
-    SET_AUTO_ATTACH = 'Target.setAutoAttach'
-    SET_DISCOVER_TARGETS = 'Target.setDiscoverTargets'
-    # Métodos experimentais
-    ATTACH_TO_BROWSER_TARGET = 'Target.attachToBrowserTarget'
-    AUTO_ATTACH_RELATED = 'Target.autoAttachRelated'
-    EXPOSE_DEV_TOOLS_PROTOCOL = 'Target.exposeDevToolsProtocol'
     GET_TARGET_INFO = 'Target.getTargetInfo'
+    GET_TARGETS = 'Target.getTargets'
+    SEND_MESSAGE_TO_TARGET = 'Target.sendMessageToTarget'
+    SET_AUTO_ATTACH = 'Target.setAutoAttach'
+    AUTO_ATTACH_RELATED = 'Target.autoAttachRelated'
+    SET_DISCOVER_TARGETS = 'Target.setDiscoverTargets'
     SET_REMOTE_LOCATIONS = 'Target.setRemoteLocations'
+    OPEN_DEV_TOOLS = 'Target.openDevTools'
+
+
+# Parameter types
+class ActivateTargetParams(TypedDict):
+    """Parameters for the activateTarget command."""
+
+    targetId: TargetID
+
+
+class AttachToTargetParams(TypedDict):
+    """Parameters for the attachToTarget command."""
+
+    targetId: TargetID
+    flatten: NotRequired[bool]
+
+
+class CloseTargetParams(TypedDict):
+    """Parameters for the closeTarget command."""
+
+    targetId: TargetID
+
+
+class ExposeDevToolsProtocolParams(TypedDict):
+    """Parameters for the exposeDevToolsProtocol command."""
+
+    targetId: TargetID
+    bindingName: NotRequired[str]
+    inheritPermissions: NotRequired[bool]
+
+
+class CreateBrowserContextParams(TypedDict):
+    """Parameters for the createBrowserContext command."""
+
+    disposeOnDetach: NotRequired[bool]
+    proxyServer: NotRequired[str]
+    proxyBypassList: NotRequired[str]
+    originsWithUniversalNetworkAccess: NotRequired[list[str]]
+
+
+class CreateTargetParams(TypedDict):
+    """Parameters for the createTarget command."""
+
+    url: str
+    left: NotRequired[int]
+    top: NotRequired[int]
+    width: NotRequired[int]
+    height: NotRequired[int]
+    windowState: NotRequired[WindowState]
+    browserContextId: NotRequired[BrowserContextID]
+    enableBeginFrameControl: NotRequired[bool]
+    newWindow: NotRequired[bool]
+    background: NotRequired[bool]
+    forTab: NotRequired[bool]
+    hidden: NotRequired[bool]
+
+
+class DetachFromTargetParams(TypedDict):
+    """Parameters for the detachFromTarget command."""
+
+    sessionId: NotRequired[SessionID]
+    targetId: NotRequired[TargetID]
+
+
+class DisposeBrowserContextParams(TypedDict):
+    """Parameters for the disposeBrowserContext command."""
+
+    browserContextId: BrowserContextID
+
+
+class GetTargetInfoParams(TypedDict):
+    """Parameters for the getTargetInfo command."""
+
+    targetId: NotRequired[TargetID]
+
+
+class GetTargetsParams(TypedDict):
+    """Parameters for the getTargets command."""
+
+    filter: NotRequired[TargetFilter]
+
+
+class SendMessageToTargetParams(TypedDict):
+    """Parameters for the sendMessageToTarget command."""
+
+    message: str
+    sessionId: NotRequired[SessionID]
+    targetId: NotRequired[TargetID]
+
+
+class SetAutoAttachParams(TypedDict):
+    """Parameters for the setAutoAttach command."""
+
+    autoAttach: bool
+    waitForDebuggerOnStart: bool
+    flatten: NotRequired[bool]
+    filter: NotRequired[TargetFilter]
+
+
+class AutoAttachRelatedParams(TypedDict):
+    """Parameters for the autoAttachRelated command."""
+
+    targetId: TargetID
+    waitForDebuggerOnStart: bool
+    filter: NotRequired[TargetFilter]
+
+
+class SetDiscoverTargetsParams(TypedDict):
+    """Parameters for the setDiscoverTargets command."""
+
+    discover: bool
+    filter: NotRequired[TargetFilter]
+
+
+class SetRemoteLocationsParams(TypedDict):
+    """Parameters for the setRemoteLocations command."""
+
+    locations: list[RemoteLocation]
+
+
+class OpenDevToolsParams(TypedDict):
+    """Parameters for the openDevTools command."""
+
+    targetId: TargetID
+
+
+# Result types
+class AttachToTargetResult(TypedDict):
+    """Result for the attachToTarget command."""
+
+    sessionId: SessionID
+
+
+class AttachToBrowserTargetResult(TypedDict):
+    """Result for the attachToBrowserTarget command."""
+
+    sessionId: SessionID
+
+
+class CloseTargetResult(TypedDict):
+    """Result for the closeTarget command."""
+
+    success: bool
+
+
+class CreateBrowserContextResult(TypedDict):
+    """Result for the createBrowserContext command."""
+
+    browserContextId: BrowserContextID
+
+
+class GetBrowserContextsResult(TypedDict):
+    """Result for the getBrowserContexts command."""
+
+    browserContextIds: list[BrowserContextID]
+
+
+class CreateTargetResult(TypedDict):
+    """Result for the createTarget command."""
+
+    targetId: TargetID
+
+
+class GetTargetInfoResult(TypedDict):
+    """Result for the getTargetInfo command."""
+
+    targetInfo: TargetInfo
+
+
+class GetTargetsResult(TypedDict):
+    """Result for the getTargets command."""
+
+    targetInfos: list[TargetInfo]
+
+
+class OpenDevToolsResult(TypedDict):
+    """Result for the openDevTools command."""
+
+    targetId: TargetID
+
+
+# Response types
+ActivateTargetResponse = EmptyResponse
+AttachToTargetResponse = Response[AttachToTargetResult]
+AttachToBrowserTargetResponse = Response[AttachToBrowserTargetResult]
+CloseTargetResponse = Response[CloseTargetResult]
+ExposeDevToolsProtocolResponse = EmptyResponse
+CreateBrowserContextResponse = Response[CreateBrowserContextResult]
+GetBrowserContextsResponse = Response[GetBrowserContextsResult]
+CreateTargetResponse = Response[CreateTargetResult]
+DetachFromTargetResponse = EmptyResponse
+DisposeBrowserContextResponse = EmptyResponse
+GetTargetInfoResponse = Response[GetTargetInfoResult]
+GetTargetsResponse = Response[GetTargetsResult]
+SendMessageToTargetResponse = EmptyResponse
+SetAutoAttachResponse = EmptyResponse
+AutoAttachRelatedResponse = EmptyResponse
+SetDiscoverTargetsResponse = EmptyResponse
+SetRemoteLocationsResponse = EmptyResponse
+OpenDevToolsResponse = Response[OpenDevToolsResult]
+
+
+# Command types
+ActivateTargetCommand = Command[ActivateTargetParams, ActivateTargetResponse]
+AttachToTargetCommand = Command[AttachToTargetParams, AttachToTargetResponse]
+AttachToBrowserTargetCommand = Command[EmptyParams, AttachToBrowserTargetResponse]
+CloseTargetCommand = Command[CloseTargetParams, CloseTargetResponse]
+ExposeDevToolsProtocolCommand = Command[
+    ExposeDevToolsProtocolParams, ExposeDevToolsProtocolResponse
+]
+CreateBrowserContextCommand = Command[CreateBrowserContextParams, CreateBrowserContextResponse]
+GetBrowserContextsCommand = Command[EmptyParams, GetBrowserContextsResponse]
+CreateTargetCommand = Command[CreateTargetParams, CreateTargetResponse]
+DetachFromTargetCommand = Command[DetachFromTargetParams, DetachFromTargetResponse]
+DisposeBrowserContextCommand = Command[DisposeBrowserContextParams, DisposeBrowserContextResponse]
+GetTargetInfoCommand = Command[GetTargetInfoParams, GetTargetInfoResponse]
+GetTargetsCommand = Command[GetTargetsParams, GetTargetsResponse]
+SendMessageToTargetCommand = Command[SendMessageToTargetParams, SendMessageToTargetResponse]
+SetAutoAttachCommand = Command[SetAutoAttachParams, SetAutoAttachResponse]
+AutoAttachRelatedCommand = Command[AutoAttachRelatedParams, AutoAttachRelatedResponse]
+SetDiscoverTargetsCommand = Command[SetDiscoverTargetsParams, SetDiscoverTargetsResponse]
+SetRemoteLocationsCommand = Command[SetRemoteLocationsParams, SetRemoteLocationsResponse]
+OpenDevToolsCommand = Command[OpenDevToolsParams, OpenDevToolsResponse]
--- pydoll/protocol/target/types.py
+++ pydoll/protocol/target/types.py
@@ -1,4 +1,36 @@
+from enum import Enum
+
 from typing_extensions import NotRequired, TypedDict
+
+from pydoll.protocol.browser.types import BrowserContextID
+from pydoll.protocol.page.types import FrameId
+
+TargetID = str
+SessionID = str
+"""Unique identifier of attached debugging session."""
+
+
+class TargetInfo(TypedDict):
+    targetId: TargetID
+    type: str
+    title: str
+    url: str
+    attached: bool
+    openerId: NotRequired[TargetID]
+    canAccessOpener: NotRequired[bool]
+    openerFrameId: NotRequired[FrameId]
+    browserContextId: NotRequired[BrowserContextID]
+    subtype: NotRequired[str]
+
+
+class FilterEntry(TypedDict, total=False):
+    """A filter used by target query/discovery/auto-attach operations."""
+
+    exclude: bool
+    type: str
+
+
+TargetFilter = list[FilterEntry]
 
 
 class RemoteLocation(TypedDict):
@@ -6,14 +38,10 @@ class RemoteLocation(TypedDict):
     port: int
 
 
-class TargetInfo(TypedDict):
-    targetId: str
-    type: str
-    title: str
-    url: str
-    attached: bool
-    openerId: NotRequired[str]
-    canAccessOpener: bool
-    openerFrameId: NotRequired[str]
-    browserContextId: NotRequired[str]
-    subtype: NotRequired[str]
+class WindowState(str, Enum):
+    """The state of the target window."""
+
+    NORMAL = 'normal'
+    MINIMIZED = 'minimized'
+    MAXIMIZED = 'maximized'
+    FULLSCREEN = 'fullscreen'
