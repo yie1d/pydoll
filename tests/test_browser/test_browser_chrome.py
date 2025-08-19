d356b3317a49e48973721f756ac1b0f03f459169
kajuberdut <patrick.shechet@gmail.com>
2025-08-19 10:43:55 -0400

    fix(browser): add google-chrome-stable path for Arch Linux AUR package
    Include `google-chrome-stable` in `browser_paths` so Arch Linux users
    with the AUR Google Chrome package are supported. This matches the
    default binary name from https://aur.archlinux.org/packages/google-chrome.

--- tests/test_browser/test_browser_chrome.py
+++ tests/test_browser/test_browser_chrome.py
@@ -131,7 +131,7 @@ class TestChromeDefaultBinaryLocation:
             ),
             (
                 'Linux',
-                ['/usr/bin/google-chrome']
+                ['/usr/bin/google-chrome', '/usr/bin/google-chrome-stable']
             ),
             (
                 'Darwin',
