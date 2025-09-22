ec4eb90b8f3a172d56d18ac7894802db8f2c96cb
cvoluj <126268939+Cvoluj@users.noreply.github.com>
2025-09-22 12:50:02 +0300

    Update pydoll/elements/mixins/find_elements_mixin.py
    Co-authored-by: Thalison Fernandes <thalisondev@gmail.com>

--- pydoll/elements/mixins/find_elements_mixin.py
+++ pydoll/elements/mixins/find_elements_mixin.py
@@ -478,7 +478,7 @@ class FindElementsMixin:
         - XPath: starts with ./, or /
         - Default: CSS_SELECTOR
         """
-        if expression.startswith('./') or expression.startswith('/') or expression.startswith('(/'):
+        if any([expression.startswith('./'), expression.startswith('/'), expression.startswith('(/')]):
             return By.XPATH
 
         return By.CSS_SELECTOR
